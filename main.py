8fc5c49746b423f1ed0b6dbe0405841e6adafd7e
Pavel Novikov <ya@pnovik.ru>
2024-05-07 23:24:35 +0300

    fix

--- main.py
+++ main.py
@@ -5,37 +5,37 @@ from src.Vacancy import Vacancy
 response = GetApiHh()
 
 while True:
-    user_vacancy: str = input("Text name vacancy for search:\n")
-    user_city: str = input("Text city in where you want to find vacancies:\n")
+    user_vacancy: str = input("Наименование вакансии для поиска:\n")
+    user_city: str = input("В каком городе ищем вакансию:\n")
     if isinstance(user_city, str) and isinstance(user_vacancy, str):
         break
-    print(f"Please, text alpha, not {user_city}")
+    print(f"Пожалуйста, напишите город {user_city}")
 
 while True:
-    user_salary = input("Text min salary for search:\n")
+    user_salary = input("На какую минимальную ЗП вы готовы:\n")
     if user_salary.isdigit():
         break
-    print(f"Please, text digit, not {user_salary}")
+    print(f"Пожалуйста, введите цифрами {user_salary}")
 
-# Get vacancies for user
+# Получаем список вакансий по заданным параметрам
 response.get_vacancy_from_api(user_vacancy)
 
 file_json = JsonSaver()
 
-# Save response to JSON
+# Сохраняем список вакансий в JSON файле
 file_json.save_file(response.all_vacancy)
 
-# Read JSON file
+# Читаем список вакансий из JSON файла
 file_vacancies = file_json.read_file()
 
-# Print vacancies for user
+# Печатываем список вакансий
 vacancy = Vacancy.get_vacancy_list(file_vacancies, user_city, int(user_salary))
 sorted_vacancies = sorted(vacancy)
 
 while True:
-    count = input("How vacancies do you want to see:\n")
+    count = input("Какие вакансии вы хотите видеть :\n")
     if count.isdigit():
         break
-    print(f"Please, text digit, not {count}")
+    print(f"Пжл, введите {count}")
 
 print(*sorted_vacancies[:int(count)])
