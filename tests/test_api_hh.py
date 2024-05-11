9d2f18ee9c2f24b668ea75247c8692f30ce3a611
Pavel Novikov <ya@pnovik.ru>
2024-05-11 18:46:34 +0300

    Fix tests

--- tests/test_api_hh.py
+++ tests/test_api_hh.py
@@ -3,19 +3,19 @@ from abc import ABC
 import pytest
 
 from config import DATA
-from src.ApiHH import ApiHH
-from src.AbstractHh import AbstractHh
+from src.GetAPIHH import GetApiHh
+from src.AbstractApiHH import AbstractGetApiHh
 
 
 def test_issubclass():
-    assert issubclass(AbstractHh, ABC)
-    assert issubclass(ApiHH, AbstractHh)
+    assert issubclass(AbstractGetApiHh, ABC)
+    assert issubclass(GetApiHh, AbstractGetApiHh)
 
 
 def test_get_vacancy_from_api():
-    vacancy1 = ApiHH('tttttt')
-    vacancy2 = ApiHH(1)
-    vacancy3 = ApiHH('python')
+    vacancy1 = GetApiHh('tttttt')
+    vacancy2 = GetApiHh(1)
+    vacancy3 = GetApiHh('python')
 
     vacancy1.get_vacancy_from_api()
     vacancy2.get_vacancy_from_api()
@@ -27,13 +27,13 @@ def test_get_vacancy_from_api():
 
 
 def test_save_info_valid():
-    vacancy1 = ApiHH('python')
+    vacancy1 = GetApiHh('python')
 
     assert isinstance(vacancy1.all_vacancy, list)
     assert len(vacancy1.all_vacancy) > 0
 
 
 def test_save_info_zero_len():
-    vacancy1 = ApiHH(1)
+    vacancy1 = GetApiHh(1)
 
     assert vacancy1.save_info() == "Vacancy not found"
